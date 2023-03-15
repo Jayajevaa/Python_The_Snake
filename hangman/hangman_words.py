af331c9107ef6954768390c453a94bd76eb66ff7
Jayajevaa <102273492+Jayajevaa@users.noreply.github.com>
2023-03-15 12:31:01 +0530

    Delete hangman directory

--- hangman/hangman_words.py
+++
@@ -1,4 +0,0 @@
-shit=['snapshot', 'public', 'campus', 'midwest', 'himself', 'chipmunk', 'talcum', 'invest', 'shellfish', 'dustpan', 'bandit', 'banquet','ambulence','patience','pencil','android','biggboss', 'aardvark','baboon','camel','lion','tiger']
-fruits=['apple', 'mango','orange','kiwi','papaya','banana']
-animals=['lion','tiger','camel','dog','cat','elephant','chipmunk']
-rules='\n------RULES------\nONE WORD WILL BE CHOSEN BY THE COMPUTER IN RANDOM\nYOU WILL BE ASKED TO ENTER A LETTER THAT U GUESS IS THERE IN THE WORD\nIF THE LETTER IS THERE IN THE WORD YOU GO FURTHER\nELSE ONE LIFE WILL BE TAKEN AWAY FROM YOU\nYOU HAVE 6 LIFES TO SAVE YOUR MAN FROM HANGING\nBEST OF LUCK\n'
